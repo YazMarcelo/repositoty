6642102b0953107c1384f383b3af01394e044d93
aluno <aluno@LAB109-12>
2017-03-13 19:10:40 -0300

    socket

---
+++ Documents/NetBeansProjects/exemplo-socket/src/exemplo/socket/ExemploSocket.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExemploSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(9000);){
+            Socket socket = server.accept();
+            Scanner scan = new Scanner(socket.getInputStream());
+        }catch (IOException ex){
+            Logger.getLogger(ExemploSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
